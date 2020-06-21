9b91bd1883ae8be5a11b8ed1ee80f1ba450885f1
tair <adibek>
2020-06-21 22:44:00 +0600

    Initial Commit

---
+++ SmartVolunteer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartVolunteer
+//
+//  Created by Таир Адибек on 6/21/20.
+//  Copyright © 2020 Таир Адибек. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
