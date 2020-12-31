9013f98b49be31cbd625d2b2418062920aa241ae
Sultan <dakanov.s@gmail.com>
2020-12-31 07:24:26 +0600

    camera

---
+++ SmartVolunteer/Create application/DSCameraHandler.swift
@@ -0,0 +1,13 @@
+//
+//  DSCameraHandler.swift
+//  SmartVolunteer
+//
+//  Created by Sultan on 12/31/20.
+//  Copyright © 2020 Таир Адибек. All rights reserved.
+//
+
+import UIKit
+
+class DSCameraHandler: NSObject {
+
+}
